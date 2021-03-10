61548465bd6959fe8ddada12dabd8592976b19e3
BhavyaBajaj <bhavyabajaj2006@gmail.com>
2021-03-10 15:52:19 +0530

    I have uploaded Images

--- sketch.js
+++ sketch.js
@@ -144,7 +144,7 @@ function draw() {
 
 function spawnClouds() {
   //write code here to spawn the clouds
-  if (frameCount % 60 === 0) {
+  if (frameCount % 80 === 0) {
     var cloud = createSprite(width+20,height-300,40,10);
     cloud.y = Math.round(random(100,220));
     cloud.addImage(cloudImage);
