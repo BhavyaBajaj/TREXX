87c627a9c690b6b7481e8587242d41423ee259a8
BhavyaBajaj <75062924+BhavyaBajaj@users.noreply.github.com>
2021-03-11 20:31:51 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -50,7 +50,7 @@ function setup() {
   trex.addAnimation("running", trex_running);
   trex.addAnimation("collided", trex_collided);
   trex.setCollider('circle',0,0,350)
-  trex.scale = 2;
+  trex.scale = 3;
   // trex.debug=true
   
   invisibleGround = createSprite(width/2,height-10,width,125);  
@@ -166,7 +166,7 @@ function spawnClouds() {
 
 function spawnObstacles() {
   if(frameCount % 60 === 0) {
-    var obstacle = createSprite(600,height-95,20,30);
+    var obstacle = createSprite(500,height-95,20,30);
     obstacle.setCollider('circle',0,0,45)
     // obstacle.debug = true
   
